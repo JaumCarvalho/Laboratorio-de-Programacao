572ce9ef91e77fc95ae1d359faf6ba468ddf2923
João Carvalho <79161378+JaumCarvalho@users.noreply.github.com>
2024-01-24 12:23:48 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,3 +6,24 @@ public class Main{
         System.out.println("O fatorial de " + numero + " é " + fatorial);
     }
 }
+
+/*
+
+public class CalculadoraFatorial {
+    private int numero;
+
+    public CalculadoraFatorial(int numero) {
+        this.numero = numero;
+    }
+
+    public int calcularFatorial() {
+        int resultado = 1;
+        for (int i = 1; i <= numero; ++i) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
+
+
+*/
